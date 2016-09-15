16a3c518b4b115a89621ef63a8d1f49105ff0829
moca127 <LifeTear@mail.com>
2016-09-15 11:53:03 +0900

    U

--- installer.sh
+++ installer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
 wget http://ci.mengcraft.com:8080/job/Nukkit/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
 wget https://raw.githubusercontent.com/moca127/ServerPack/master/start.sh
 wget https://raw.githubusercontent.com/moca127/ServerPack/master/loop.sh
